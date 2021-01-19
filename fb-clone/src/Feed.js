58a3b56285e7bf663718d7d67904cc593941e180
Mumbi <veronicmuthiga@gmail.com>
2021-01-19 15:02:41 +0300

    Add The post Category

--- fb-clone/src/Feed.js
+++ fb-clone/src/Feed.js
@@ -2,13 +2,27 @@ import React from 'react'
 import './Feed.css'
 import StoryReel from './StoryReel'
 import MessageSender from './MessageSender'
+import Post from './Post'
+import image2 from './images/image2.jpg'
+
 function Feed() {
+    
     return (
         <div className='feed'>
             {/*StoryReel*/}
              <StoryReel></StoryReel>
             {/*MessageSender*/}
             <MessageSender></MessageSender>
+            {/*Post*/}
+            <Post
+            profilePic= 'https://scontent.ffjr1-2.fna.fbcdn.net/v/t1.0-9/52047628_2064911377141850_3925252065869168640_o.jpg?_nc_cat=109&ccb=2&_nc_sid=e3f864&_nc_ohc=NPf5oXD9BFEAX9YMF2J&_nc_ht=scontent.ffjr1-2.fna&oh=b7b0a0bf752472471ba30561579072dd&oe=60255EDF'
+            message="Wow this works"
+            username="Veny Keller"
+            image='https://scontent.ffjr1-2.fna.fbcdn.net/v/t1.0-9/52047628_2064911377141850_3925252065869168640_o.jpg?_nc_cat=109&ccb=2&_nc_sid=e3f864&_nc_ohc=NPf5oXD9BFEAX9YMF2J&_nc_ht=scontent.ffjr1-2.fna&oh=b7b0a0bf752472471ba30561579072dd&oe=60255EDF'
+            ></Post>
+            <Post></Post>
+            <Post></Post>
+
 
         </div>
     )

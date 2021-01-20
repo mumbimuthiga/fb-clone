34ca4dc7515f507f560800a8efa2815965e9184a
Mumbi <veronicmuthiga@gmail.com>
2021-01-20 13:29:25 +0300

    Add the Post.js and update the Feed.css

--- fb-clone/src/Post.js
+++ fb-clone/src/Post.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Avatar } from '@material-ui/core';
+import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
+import NearMeIcon from '@material-ui/icons/NearMe';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import './Post.css'
+
 
 function Post({profilePic ,image,username,timestamp,message}) {
     return (
@@ -20,6 +26,25 @@ function Post({profilePic ,image,username,timestamp,message}) {
         </div>
         <div className="post__image">
         <img src={image} alt=""></img>
+        </div>
+        <div className="post__options">
+        <div className="post__option">
+        <ThumbUpIcon> </ThumbUpIcon>
+        <p>Likes</p>
+       
+     </div>
+     <div className="post__option">
+     <ChatBubbleOutlineIcon> </ChatBubbleOutlineIcon>
+     <p>Comment</p>
+     </div>
+     <div className="post__option">
+     <NearMeIcon></NearMeIcon>
+     <p>Share</p>
+     </div>
+     <div className="post__option">
+     <AccountCircleIcon></AccountCircleIcon>
+     <ExpandMoreIcon></ExpandMoreIcon>
+     </div>
         </div>
             
         </div>
